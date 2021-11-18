8197d7176f7ce6c58637b0d1aa6e9d5f5dc2978d
andy.eilers <andy.eilers@asurion.com>
2021-11-18 09:55:14 -0600

    added register and logout

--- src/components/Repairs.js
+++ src/components/Repairs.js
@@ -1,23 +1,35 @@
 //Imports react and required listing from outside modules
-import React from "react"
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
 //no ".js" needed in React
-import { CustomerList } from "./customers/CustomerList";
-import { EmployeeList } from "./employees/EmployeeList";
-import { TicketList } from "./serviceTickets/TicketList";
+import { NavBar } from "./nav/NavBar";
+import { ApplicationViews } from "./ApplicationViews";
+import "./Repairs.css";
+import { Login } from "./auth/Login";
+import { Register } from "./auth/Register";
 //exporting Repairs to browser in HTML
-export const Repairs = () => {
-    return (
-        /*Fragmenting elements, React can only read one element, so headings
-        are encapsulated in one fragmented element*/
-        <>
-        <h1>Honey Rae's Repair Shop</h1>
-        
-        <h2>Customer List</h2>
-        <CustomerList />
-        <h2>Employee List</h2>
-        <EmployeeList />
-        <h2>Service Tickets</h2>
-        <TicketList />
-        </>
-    )
-}
+export const Repairs = () => (
+    <>
+      <Route
+        render={() => {
+          if (localStorage.getItem("honey_customer")) {
+            return (
+              <>
+                <NavBar />
+                <ApplicationViews />
+              </>
+            );
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+  
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/register">
+        <Register />
+      </Route>
+    </>
+  );
---
+++ src/components/nav/NavBar.js
@@ -0,0 +1,28 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./NavBar.css"
+
+//Link generates NavBars
+export const NavBar = () => {
+    return (
+        <ul className="navbar">
+            <li className="navbar__item active">
+                <Link className="navbar__link" to="/employees">Employees</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/tickets">Tickets</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="/customers">Customers</Link>
+            </li>
+            <li className="navbar__item">
+                <Link className="navbar__link" to="#"
+                    onClick={
+                        (localStorage.removeItem("honey_customer"))
+                    }>
+                    Logout
+                </Link>
+            </li>
+        </ul>
+    )
+}
